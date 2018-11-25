32b1bed929428a56e527fe992d2248d53af658dc
tbsund <39915676+tbsund@users.noreply.github.com>
2018-11-25 11:59:24 -0600

    Add files via upload

---
+++ 850 final.R
@@ -0,0 +1,37 @@
+
+#1 is 3 parts and 30 points writing r code
+#2 is 4 parts and 24 points writing r code
+#3 is short answer and 12 points no r code needed
+#4 is 5 short answers and 34 points with some r code
+#5 is ec for 3 points
+
+#topics
+#write function
+#regression
+#  access certain components
+#  summary function
+#data management
+#plot
+#  curve
+#object oriented programming
+#graphics
+#    legend
+ #   text
+  #  identify
+   # par
+    #how to get math and greek letters
+    #color coding data points
+    #single var plots
+#control structures
+  #  if else
+   # if
+    #for
+    #apply
+    #monte carlo simulation
+#summarizing and managing
+#  merge
+#  reshape
+#matrix algebra
+#  matrix
+#  vectors and matrices
+#additional items
